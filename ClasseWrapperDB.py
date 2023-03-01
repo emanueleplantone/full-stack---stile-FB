0ba6932e6fea260737912f7a21a7cb05df8fb408
emanueleplantone <plantone.emanuele@iisgalvanimi.edu.it>
2023-03-01 09:40:46 +0000

    commit

---
+++ ClasseWrapperDB.py
@@ -0,0 +1,142 @@
+#classe wrapper
+#funzioni di connetti e di disconnetti + varie
+#le variabili di istanza sono le 4 variabili per la connetti
+#invece la connetti è una variabile di classe 
+
+#importo il modulo 
+import pymssql
+
+class WrapperDB:
+    
+    conn = 0
+    
+    #def __init__(self, server="192.168.40.16\\SQLEXPRESS", user="CRD2122", \
+    def __init__(self, server="213.140.22.237\\SQLEXPRESS", user="CRD2122", \
+               password="xxx123##", database="CRD2122"):
+        self._server=server
+        self._user=user
+        self._password=password
+        self._database=database
+        
+        
+    def connetti(self):
+        #connessione
+        try:
+            WrapperDB.conn = pymssql.connect(server = self._server, user = self._user, \
+                        password = self._password, database = self._database)
+            #print(f"\nConnessione effettuata! (DB: {self._database})\n")
+            return WrapperDB.conn	
+
+        except:
+            print(f"\nConnessione NON riuscita! (DB: {self._database})\n")
+            return 0
+        
+            
+    def disconnetti(self, co):
+        #disconnessione	
+        try:
+            co.close()
+            #print(f"\nCHIUSURA connessione! (DB: {self._database})\n")
+            
+        except:
+            print(f"\nCHIUSURA connessione NON riuscita! (DB: {self._database})\n")
+            return 0
+        
+
+    def elencoPost(self, as_dict = False):
+        #restituisce una lista di tuple se as_dict = False
+        #altrimenti restituisce una lista di coppie chiave/valore (dictionary)
+        conn = self.connetti()
+        lista = []
+        try:
+            cur = conn.cursor(as_dict = as_dict)
+            sql = "SELECT Id, Autore, Testo, [Like] FROM PLAN_FB_Post ORDER BY [Like] DESC"
+            cur.execute(sql)
+            lista = cur.fetchall()
+        except:
+            err = "Houston abbiamo un problema..."
+            print(f"[elencoPost] {err}")
+        self.disconnetti(conn)
+        return lista
+
+    
+    def singoloPost(self, id):
+        #restituisce un singolo post
+        ret = {}
+        conn = self.connetti()
+        try:
+            cursore = conn.cursor(as_dict = True)
+            sql = f"""
+                SELECT Id, Autore, Testo, [Like] 
+                FROM PLAN_FB_Post 
+                WHERE id = {id}   
+                """
+            cursore.execute(sql)
+            ret = cursore.fetchone()
+        except:
+            err = "Houston abbiamo un problema..."
+            print(f"[singoloPost] {err}")
+        self.disconnetti(conn)
+        return ret    
+
+    
+    #def inserisciPost(self, autore, testo):
+    def inserisciPost(self, parametri):
+        #inserisce un nuovo post
+        #parametri: (autore, testo)
+        try:
+            c = self.connetti() 
+            cursore = c.cursor()
+            sql = "INSERT INTO PLAN_FB_Post (Autore, Testo) VALUES (%s , %s)"
+            cursore.execute(sql, parametri)
+            c.commit()
+            #print("INSERIMENTO POST AVVENUTO")
+            self.disconnetti(c)
+            return True            
+        except:
+            #print("\INSERIMENTO POST/i: Si sono verificati degli errori!")
+            self.disconnetti(c)
+            return False
+
+    def daiLikeAPost(self, id, is_like = True):
+        #mette like a post
+        #se is_like è False toglie un like
+        try:
+            c = self.connetti() 
+            cursore = c.cursor()
+            sql = "UPDATE PLAN_FB_Post SET [Like] = "
+            if is_like == True: 
+                sql += "[Like] + 1 "
+            else:
+                sql += "[Like] - 1 "
+            sql += "WHERE id = %d"
+            cursore.execute(sql, id)
+            c.commit()
+            #print("LIKE A POST AVVENUTO")
+            self.disconnetti(c)
+            return True                        
+        except:
+            #print("\LIKE A POST/i: Si sono verificati degli errori!")
+            self.disconnetti(c)
+            return False
+
+
+    def eliminaPost(self, id):
+        #elimina un post
+        try:
+            c = self.connetti() 
+            cursore = c.cursor()
+            sql = "DELETE PLAN_FB_Post WHERE id = %d"
+            cursore.execute(sql, id)
+            c.commit()
+            #print("ELIMINA POST AVVENUTO")
+            self.disconnetti(c)
+            return True            
+            
+        except:
+            #print("\ELIMINA POST/i: Si sono verificati degli errori!")
+            self.disconnetti(c)
+            return False
+
+    
+
---
+++ UsoClasseWrapperDB.py
@@ -0,0 +1,45 @@
+#Programma che usa un oggetto di WrapperDB
+from ClasseWrapperDB import WrapperDB         
+
+# CREO OGGETTO WRAPPER--------------------------------------------------------
+#wrp = WrapperDB()
+wrp = WrapperDB("213.140.22.237\\SQLEXPRESS", "CRD2122", "xxx123##", "CRD2122")
+
+print()
+print("\n***** TEST elencoPost *****")
+print(wrp.elencoPost(as_dict = True))
+print("*****************************")
+
+
+print()
+print("\n***** TEST singoloPost *****")
+print(wrp.singoloPost(1))
+print("******************************")
+
+
+print()
+print("\n***** TEST daiLikeAPost *****")
+print("Prima:")
+print(wrp.singoloPost(1))
+wrp.daiLikeAPost(1)
+print("Dopo:")
+print(wrp.singoloPost(1))
+print("*******************************")
+
+
+#print()
+#print("\n***** TEST inserisciPost *****")
+#parametri = ("Omino del meteo", "Ieri nevicava!!!")
+#wrp.inserisciPost(parametri)
+#print(wrp.elencoPost(as_dict = True))
+#print("********************************")
+
+
+#print()
+#print("\n***** TEST eliminaPost *****")
+#print("Prima:")
+#print(wrp.elencoPost(as_dict = True))
+#wrp.eliminaPost(3)
+#print("Dopo:")
+#print(wrp.elencoPost(as_dict = True))
+#print("******************************")
